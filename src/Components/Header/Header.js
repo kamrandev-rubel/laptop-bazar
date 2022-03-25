24df22eaa3c78b2b8faecc432564c2d73bc0e9cf
kamrandev-rubel <kamranuddin.rubel1@gmail.com>
2022-03-26 02:38:21 +0300

    set navbar hover and body background color

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,15 +4,17 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div className='header'>
-            <div className="logo">
-                <img src={logo} alt="" />
-            </div>
-            <div className="navigation">
-                <a href="/home">Home</a>
-                <a href="/shop">Shop</a>
-                <a href="/about">About</a>
-                <a href="/contacts">Contacts</a>
+        <div className='header-container'>
+            <div className='header'>
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="navigation">
+                    <a href="/home">Home</a>
+                    <a href="/shop">Shop</a>
+                    <a href="/about">About</a>
+                    <a href="/contacts">Contacts</a>
+                </div>
             </div>
         </div>
     );
